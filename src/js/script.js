d8cd2a94f29b61a483cd518f8cadd6cd95d1e331
BooMooN <mael.brungard44320@gmail.com>
2025-12-02 15:33:10 +0100

    COMMIT

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 // *** LIGHTBOX ***
+const lightBox = document.querySelector("#lightbox");
+const images = document.querySelectorAll('.listimages');
+const lbimg = lightBox.querySelector('img');
+
+images.forEach(e => {
+    const url = e.dataset.fullImg;
+    e.addEventListener('click', () => {
+        lbimg.src = url;
+        lightBox.showModal();
+    });
+});
+
+lightBox.addEventListener('click', () => {
+    lightBox.close();
+});
+
